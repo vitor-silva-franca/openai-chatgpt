6d9cfeadb127b265981f6f10c4761574ffd1e0e0
vitor-silva-franca <sf_vitor@outlook.com>
2025-03-15 13:11:46 -0300

    Init

---
+++ src/main/java/com/vitorsilvafranca/openai_chatgpt/OpenAiChatGptRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.vitorsilvafranca.openai_chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiChatGptRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAiChatGptRestApiApplication.class, args);
+	}
+
+}
